472e79cf187aa919b7cceef4bbcf3d598d8431cd
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-29 12:36:29 +0530

    added

--- DevTool-Error/dev.js
+++ DevTool-Error/dev.js
@@ -1,6 +1,8 @@
 /**
  * error 1. >>
- *        react_devtools_backend.js:4012 Warning: Cannot update a component     (`BrowserRouter`) while rendering a different component (`SocialLogin`). To locate the bad setState() call inside `SocialLogin`, follow the stack trace as described in https://reactjs.org/link/setstate-in-render
+ *        react_devtools_backend.js:4012 Warning: Cannot update a component     (`BrowserRouter`) while rendering a different component
+ *  (`SocialLogin`). To locate the bad setState() call inside 
+ * `SocialLogin`, follow the stack trace as described in https://reactjs.org/link/setstate-in-render
  * 
  * Solved:>>
  *   use---> useEffect(()=>{
